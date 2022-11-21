a46935bb66ce49112ecaafe7d25a623fc5bd5400
Achyutha <aachyutha2@gmail.com>
2022-11-21 19:11:39 +0530

    Adds health check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+use std::net::SocketAddr;
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health_check", get(health_check));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
