d6816711450ff6a296f300dbe1fa7677171c7f5b
Achyutha <aachyutha2@gmail.com>
2023-04-26 12:57:14 +0530

    fix: fixes the function name

--- api-server/src/routes/health_check.rs
+++ api-server/src/routes/health_check.rs
@@ -5,13 +5,13 @@ use utils::AppState;
 
 use crate::common::Responder;
 
-pub async fn mysql_test(conn: &sqlx::MySqlPool) -> Result<(), anyhow::Error> {
+pub async fn mysql_check(conn: &sqlx::MySqlPool) -> Result<(), anyhow::Error> {
     sqlx::query!("SELECT now() as now").fetch_one(conn).await?;
     Ok(())
 }
 
 pub async fn health_check(State(app_state): State<AppState>) -> Responder<()> {
-    if mysql_test(&app_state.db).await.is_err() {
+    if mysql_check(&app_state.db).await.is_err() {
         error!("Database connection failed!");
         return Responder::create_response((), StatusCode::INTERNAL_SERVER_ERROR, None);
     }
