bfffd4b518208a78dab871f4bca64b4f95fa5694
Achyutha <aachyutha2@gmail.com>
2023-04-26 10:42:24 +0530

    chore: refactors app_state to get from utils

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -2,3 +2,26 @@ pub mod app_settings;
 pub mod config;
 
 pub use app_settings::*;
+use sqlx::MySqlPool;
+
+use crate::config::Settings;
+use anyhow::{Context, Result};
+
+pub async fn get_app_state() -> Result<AppState, anyhow::Error> {
+    let config = Settings::get_configuration()
+        .with_context(|| "Unable to read the configuration file!".to_string())?;
+
+    let connection_pool = MySqlPool::connect(&config.database.connection_string())
+        .await
+        .with_context(|| {
+            format!(
+                "Unable to connect to the database {}",
+                &config.database.database_name
+            )
+        })?;
+
+    Ok(AppState {
+        db: connection_pool,
+        config: config.clone(),
+    })
+}
